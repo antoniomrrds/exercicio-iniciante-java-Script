4d432ffe61772c0005e11fabdfbe4d376ce93f8f
antonio <antoniomarcos.amrrds@gmail.com>
2020-12-17 13:07:14 -0300

    Exercicio 9
    acabado

--- 09-classificacaoDeAluno.js
+++ 09-classificacaoDeAluno.js
@@ -1,20 +1,36 @@
 const classificacaoAluno = function (nota) {
     if (nota >= 0 && nota <= 100) {
-        // if (nota < 38) { 
-        //     console.log("Aluno Reprovado !!!")
-        // }
+        if (nota < 38) { 
+           return   console.log("Aluno Reprovado !!!")
+         }
         //calculoDoMultiplo de 5
-        for (let i = 0; i <)
-            if (nota % 5 == 0) {
-                console.log("Verdadeiro")
-            } else {
-                console.log("Falso")
+        let multiploDe5
+        let arredondamento 
+        let cont = nota;
+        do { 
+            if (cont % 5 == 0) {
+                multiploDe5 = cont;
+                break;
             }
+            cont ++;
+        } while (true)
+        
+        arredondamento =  multiploDe5 - nota 
+        if (arredondamento < 3) { 
+            nota = multiploDe5;
+        }
+        console.log(nota)
+        if (nota >= 40) {
+            
+            return console.log("Aluno Aprovado")
 
+         }
+        
+      
     } else {
         console.log("Digite uma Nota Valida de 0 a 100 !!!")
     }
 
 };
 
-classificacaoAluno(3)
+classificacaoAluno(-1)
