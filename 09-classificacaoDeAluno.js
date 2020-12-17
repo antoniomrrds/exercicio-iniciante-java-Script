583c1afb4d63ec8453932fab5074463c934709db
antonio <antoniomarcos.amrrds@gmail.com>
2020-12-17 00:19:50 -0300

    09 - classificacao de aluno
    classificacao de Aluno

---
+++ 09-classificacaoDeAluno.js
@@ -0,0 +1,20 @@
+const classificacaoAluno = function (nota) {
+    if (nota >= 0 && nota <= 100) {
+        // if (nota < 38) { 
+        //     console.log("Aluno Reprovado !!!")
+        // }
+        //calculoDoMultiplo de 5
+        for (let i = 0; i <)
+            if (nota % 5 == 0) {
+                console.log("Verdadeiro")
+            } else {
+                console.log("Falso")
+            }
+
+    } else {
+        console.log("Digite uma Nota Valida de 0 a 100 !!!")
+    }
+
+};
+
+classificacaoAluno(3)
