92b389a1d2255b1f6096a0c7b3bc4ff1e5cd3a5f
Marcos Antonio <62305370+antoniomrrds@users.noreply.github.com>
2022-03-12 01:14:35 -0300

    Update concatMult.js

--- concatMult.js
+++ concatMult.js
@@ -1,10 +1,15 @@
 
 //Concactenadodor de String multiplicado pe
-const concatMult = (word,el)=>{
-let res =''
-let juncao = Array.from({length: el}, n => res += word)
-return res || 'ana'
+const concatMult = (word, el) => {
+    if (word.length == 0 || !word.trim()) {
+        return 'Digite uma Palavra !!!'
+    }
+    else {
+        let res = ''
+        Array.from({ length: el }, n => res += word)
+        return (el == undefined || el == 0) ? wordStr : res
+    }
 }
-console.log(concatMult('ana',2))
 
-
+console.log(concatMult('antonio', 2))
+console.log(concatMult('', 2))
