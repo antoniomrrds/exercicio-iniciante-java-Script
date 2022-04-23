acc823829b13fc2c73d1dde3f64be3cf7b3ff595
Marcos Antonio <62305370+antoniomrrds@users.noreply.github.com>
2022-04-23 09:23:26 -0300

    Consegui resolver usando programcao funcional
    Estou melhorando

---
+++ Algoritimo intermediario/everythingBeTrue.js
@@ -0,0 +1,15 @@
+const truthCheck = (collection, pre) => collection.every(n=>n[pre])
+
+const r = [
+  truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "name")
+, 
+truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "role"),
+truthCheck([{name: "Pikachu", number: 25, caught: 3}, {name: "Togepi", number: 175, caught: 1}], "number"),
+truthCheck([{name: "Pikachu", number: 25, caught: 3}, {name: "Togepi", number: 175, caught: 1}, {name: "MissingNo", number: NaN, caught: 0}], "caught"),
+truthCheck([{name: "Pikachu", number: 25, caught: 3}, {name: "Togepi", number: 175, caught: 1}, {name: "MissingNo", number: NaN, caught: 0}], "number"),
+truthCheck([{name: "Quincy", username: "QuincyLarson"}, {name: "Naomi", username: "nhcarrigan"}, {name: "Camperbot"}], "username"),
+truthCheck([{name: "freeCodeCamp", users: [{name: "Quincy"}, {name: "Naomi"}]}, {name: "Code Radio", users: [{name: "Camperbot"}]}, {name: "", users: []}], "users")
+]
+for (const iterator of r) {
+      console.log(iterator)
+}
